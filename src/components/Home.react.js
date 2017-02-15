39c225670cdbdead8a1190b7351eed817d35053d
Kayode Adeniyi <kayode.adeniyi@andela.co>
2017-02-15 12:38:29 +0100

    Initial commit

---
+++ src/components/Home.react.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class  extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <h1>Life Ray Issues</h1>
+      </div>
+    )
+  }
+}
---
+++ src/components/Home.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import expect from 'expect'
+import { shallow } from 'enzyme'
+
+import Home from './Home.react'
+
+const wrapper = shallow(<Home />)
+
+describe('Home', () => {
+  it('contains the right text', () => {
+    expect(wrapper.find('h1').text()).toEqual('Life Ray Issues')
+  });
+});
